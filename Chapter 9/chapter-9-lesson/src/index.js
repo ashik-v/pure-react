22d2b3e9e932db802f2b3514b134fad4427d1977
Ashik Varghese <ashikv@nulogy.com>
2022-08-02 16:58:15 -0600

    Complete lesson

--- Chapter 9/chapter-9-lesson/src/index.js
+++ Chapter 9/chapter-9-lesson/src/index.js
@@ -3,15 +3,28 @@ import ReactDOM from 'react-dom'
 
 const NavBar = () => {
   return (
-    <>
-      <NavBarItem>Home</NavBarItem>
-      <NavBarItem>Contact</NavBarItem>
-      <NavBarItem>About</NavBarItem>
-    </>
+    <Nav>
+      <NavBarItem url="/">Home</NavBarItem>
+      <NavBarItem url="/about">About</NavBarItem>
+      <NavBarItem url="/contact">Contact</NavBarItem>
+    </Nav>
   )
 }
 
-const NavBarItem = ({children}) => {
-  return <a href={`/${children}`}>{children} | </a>
+const Nav = ({children}) => {
+  let items = React.Children.toArray(children)
+
+  for(let i = 1; i < items.length; i+=2) {
+    items.splice(
+      i,
+      0,
+      <span> | </span>
+    )
+  }
+
+  return <div>{items}</div>
 }
+
+const NavBarItem = ({url, children}) => <a href="{url}">{children}</a>
+
 ReactDOM.render(<NavBar />, document.querySelector('#root'))
