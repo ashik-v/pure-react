76890aaa34735afdb3adefc673ac53943c700d44
Ashik Varghese <ashikv@nulogy.com>
2022-08-19 22:02:05 -0400

    Use classes to manage state

---
+++ Chapter 11/classes-for-state-management/src/index.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+// function handleAction(event) {
+//   console.log('Child did:', event)
+// }
+
+// function Parent() {
+//   return <Child onAction={handleAction} />
+// }
+
+class CountingParent extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      actionCount: 0,
+    }
+
+    this.handleAction = this.handleAction.bind(this)
+  }
+
+  handleAction(action) {
+    this.setState({
+      actionCount: this.state.actionCount + 1,
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <Child onAction={this.handleAction} />
+        <p>Clicked {this.state.actionCount} times</p>
+      </div>
+    )
+  }
+}
+
+function Child({onAction}) {
+  return <button onClick={onAction}>Click me!</button>
+}
+
+ReactDOM.render(<CountingParent />, document.querySelector('#root'))
