79d8782c5270279dc631725a494135ca35ecdfb6
Ashik Varghese <ashikv@nulogy.com>
2022-08-22 09:49:29 -0400

    Read state successfully

--- Chapter 11/classes-for-state-management-exercises/src/index.js
+++ Chapter 11/classes-for-state-management-exercises/src/index.js
@@ -4,27 +4,34 @@ import ReactDOM from 'react-dom'
 function House() {
   return (
     <div>
-      <Room />
-      <Room />
-      <Room />
-      <Room />
+      <Room id="kitchen" label="Kitchen" />
+      <Room id="livingRoom" label="Living Room" />
+      <Room id="bathroom" label="Bathroom" />
+      <Room id="bedroom" label="Bedroom" />
     </div>
   )
 }
 
 class Room extends React.Component {
-  state = {
-    kitchen: true,
-    livingRoom: false,
-    bathroom: true,
-    bedroom: false,
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      kitchen: true,
+      livingRoom: false,
+      bathroom: true,
+      bedroom: false,
+    }
   }
 
   render() {
     return (
-      <div>
-        <LightSwitch />
-        <span>The light is currently __</span>
+      <div>{this.props.label}
+        <div>
+          <LightSwitch />
+          <span>The light is currently {this.state[this.props.id] ? "on" : "off"}</span>
+        </div>
       </div>
     )
   }
