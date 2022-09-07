17e088edb166d110bc04dfabc5a746e459e603df
Ashik Varghese <ashikv@nulogy.com>
2022-09-06 22:06:52 -0400

    Refactor: Convert to a functional component

--- Chapter 14/use-state/src/index.js
+++ Chapter 14/use-state/src/index.js
@@ -1,23 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-class OneTimeButton extends React.Component {
-  state = {
-    clicked: false
+function OneTimeButton({onClick}) {
+  const [clicked, setClicked] = React.useState(false)
+
+  const handleClick = () => {
+    onClick()
+    setClicked(true)
   }
 
-  handleClick = () => {
-    this.props.onClick()
-    this.setState({ clicked: true })
-  }
-
-  render() {
-    return (
-      <button onClick={this.handleClick} disabled={this.state.clicked}>
-        You can only click me once
-      </button>
-    )
-  }
+  return (
+    <button onClick={handleClick} disabled={clicked}>
+      You can only click me once
+    </button>
+  )
 }
 
 ReactDOM.render(
