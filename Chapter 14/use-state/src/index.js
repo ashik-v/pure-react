a1da246bbcf7b0fa17e8faa7da5fa76985a8b8fc
Ashik Varghese <ashikv@nulogy.com>
2022-09-10 19:02:45 -0400

    Implement a class component

--- Chapter 14/use-state/src/index.js
+++ Chapter 14/use-state/src/index.js
@@ -2,34 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 class OneTimeButtonClass extends React.Component {
-  state = {
-    clicked: false,
-  }
+  state = { clicked: false }
 
   handleClick = () => {
-    this.setState({clicked: true})
+    this.setState({ clicked: true })
     this.props.onClick()
   }
 
   render() {
-    return (
-      <button onClick={this.handleClick} disabled={this.state.clicked}>One time click only</button>
-    )
+    return <button onClick={this.handleClick} disabled={this.state.clicked}>one time click only</button>
   }
-}
-
-function OneTimeButtonFunction({onClick}) {
-  const [clicked, setClicked] = React.useState(false)
-
-  const handleClick = () => {
-    setClicked(true)
-    onClick()
-  }
-
-
-  return (
-      <button onClick={handleClick} disabled={clicked}>One time click only</button>
-    )
 }
 
 ReactDOM.render(
