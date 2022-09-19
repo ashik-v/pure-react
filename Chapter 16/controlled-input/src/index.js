94423f6e73d4f1837424db1e0bf47ecb9125ea59
Ashik Varghese <ashikv@nulogy.com>
2022-09-19 09:29:54 -0400

    Implement a dead simple uncontrolled input

--- Chapter 16/controlled-input/src/index.js
+++ Chapter 16/controlled-input/src/index.js
@@ -34,7 +34,37 @@ const NoNumbersInput = () => {
   return <input type="text" value={text} onChange={handleChange} />
 }
 
+// examples of uncontrolled inputs - uncontrolled as it manages it's own internal state. if you're going to want to change this initial value, best to convert this to a controlled input instead
+
+// there are two ways to get a value from an uncontrolled input
+// first, set up an onChange prop
+const EasyUncontrolledInput = ({placeholder, onChange}) => {
+  return (
+  <>
+    <input type="text" placeholder={placeholder} onChange={onChange}/>
+  </>
+  )
+}
+
+// and the parent can be updated on any changes; similar effort to a controlled input though
+const UncontrolledListener = () => {
+  const [child, setChild] = useState('')
+
+  const handleChange = (event) => {
+    setChild(event.target.value)
+  }
+
+  return (
+    <>
+      <div>Child's value is {child}</div>
+      <EasyUncontrolledInput placeholder="type here..." onChange={handleChange}/>
+    </>
+  )
+}
+
+// second, you can use the useRef hook which will give you access to the input's underlying DOM node
+
 ReactDOM.render(
-  <NoNumbersInput />,
+  <UncontrolledListener />,
   document.querySelector('#root')
 )
