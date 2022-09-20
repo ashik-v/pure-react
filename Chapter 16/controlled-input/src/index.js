f8a2fb97c45dbe82313621e6ca51865825bb6807
Ashik Varghese <ashikv@nulogy.com>
2022-09-19 22:20:28 -0400

    Implement uncontrolled inputs

--- Chapter 16/controlled-input/src/index.js
+++ Chapter 16/controlled-input/src/index.js
@@ -1,12 +1,12 @@
 import React, {useState, useRef} from 'react'
 import ReactDOM from 'react-dom'
 
-// controlled input exmaples
+// controlled input examples
 
 const SimpleControlledInput = () => {
   const [text, setText] = useState('')
 
-  const handleChange = (event) => {
+  const handleChange = () => {
     setText(event.target.value)
   }
 
@@ -21,14 +21,57 @@ const SimpleControlledInput = () => {
 const MoreSophisticatedControlledInput = () => {
   const [text, setText] = useState('')
 
-  const handleChange = (event) => {
+  const handleChange = () => {
     setText(event.target.value.replace(/[0-9]/g, ''))
   }
 
   return <input value={text} onChange={handleChange} />
 }
 
+// uncontrolled inputs
+
+// simple implementation
+const SimpleUncontrolledInput = () => {
+  return <input placeholder="type here..." />
+}
+
+// keeping track of an uncontrolled input via props
+const UncontrolledInputListener = () => {
+  const [text, setText] = useState('')
+
+  const handleChange = () => {
+    setText(event.target.value)
+  }
+
+  return(
+    <>
+      <UncontrolledInput onChange={handleChange}/>
+      <div>The child's value is {text}</div>
+    </>
+  )
+}
+
+const UncontrolledInput = ({onChange}) => {
+  return <input onChange={onChange} />
+}
+
+// keeping track of an uncontrolled input using the useRef hook
+const RefInput = () => {
+  const input = useRef()
+
+  const showRef = () => {
+    alert(`The ref value is ${input.current.value}`)
+  }
+
+  return (
+    <>
+      <input ref={input} />
+      <button onClick={showRef}>Alert the ref</button>
+    </>
+  )
+}
+
 ReactDOM.render(
-  <MoreSophisticatedControlledInput />,
+  <RefInput />,
   document.querySelector('#root')
 )
