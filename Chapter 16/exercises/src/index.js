51364273787369d4ec2a87c45b6349674912096d
Ashik Varghese <ashikv@nulogy.com>
2022-09-21 22:34:51 -0400

    Complete exercise 1 and 2

--- Chapter 16/exercises/src/index.js
+++ Chapter 16/exercises/src/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useRef} from 'react'
 import ReactDOM from 'react-dom/client'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
@@ -7,24 +7,47 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
 
 const Hello = () => {
   const [name, setName] = useState({
-    firstName: "",
-    lastName: "",
+    firstName: '',
+    lastName: '',
   })
 
-  const handleChange = (event) => {
+  const handleChange = () => {
     setName({
       ...name,
-      [event.target.id]: event.target.value,
+      [event.target.id]: event.target.value
     })
   }
 
   return (
     <>
-      <input type="text" id="firstName" placeholder="first name" onChange={handleChange} />
-      <input type="text" id="lastName" placeholder="last name" onChange={handleChange} />
+      <input id="firstName" type="text" placeholder="first name" onChange={handleChange} />
+      <input id="lastName" type="text" placeholder="last name" onChange={handleChange} />
       <div>{`Hello, ${name.firstName} ${name.lastName}`}</div>
     </>
   )
 }
+
+// exercise 2
+
+const RefHello = () => {
+  const firstName = useRef()
+  const lastName = useRef()
+
+  const handleClick = () => {
+    alert(`Hello, ${firstName.current.value} ${lastName.current.value}`)
+  }
+
+  return(
+    <>
+      <input type="text" placeholder="first name" ref={firstName} />
+      <input type="text" placeholder="last name" ref={lastName} />
+      <button onClick={handleClick}>Submit</button>
+    </>
+  )
+}
+
+// exercise 3
+
+
 
 root.render(<Hello />)
