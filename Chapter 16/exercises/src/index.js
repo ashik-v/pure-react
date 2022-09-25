2b0a2ba87fb55bdf2e497b7cf9f27ff11902a9f3
Ashik Varghese <ashikv@nulogy.com>
2022-09-24 21:15:18 -0400

    Complete exercise 3

--- Chapter 16/exercises/src/index.js
+++ Chapter 16/exercises/src/index.js
@@ -48,6 +48,125 @@ const RefHello = () => {
 
 // exercise 3
 
+const RadioButtonForm = () => {
+  const [letter, setLetter] = useState('')
 
+  const handleChange = () => {
+    setLetter(event.target.value)
+  }
 
-root.render(<Hello />)
+  return (
+    <form>
+      <input
+        type="radio"
+        value="a"
+        checked={letter === 'a'}
+        onChange={handleChange}
+      />
+      <input
+        type="radio"
+        value="b"
+        checked={letter === 'b'}
+        onChange={handleChange}
+      />
+      <input
+        type="radio"
+        value="c"
+        checked={letter === 'c'}
+        onChange={handleChange}
+      />
+    </form>
+  )
+}
+
+const PizzaForm = () => {
+  const [formState, setFormState] = useState({
+    size: '',
+    topping: 'pepperoni',
+    glutenFree: false,
+    notes: ''
+  })
+
+  const handleChange = (event) => {
+    setFormState({
+      ...formState,
+      [event.target.id]: event.target.value
+    })
+  }
+
+  const handleCheck = () => {
+    setFormState({
+      ...formState,
+      [event.target.id]: !formState[event.target.id]
+    })
+  }
+
+  const handleSubmit = () => {
+    const glutenFree = formState.glutenFree ? ' gluten-free ' : ' '
+    alert(`you are ordering a ${formState.size}${glutenFree}${formState.topping} pizza. We will try our best to make it ${formState.notes}`)
+  }
+
+  return(
+    <>
+      <div>Order your pizza</div>
+      <div>Size
+        <input
+          id="size"
+          type="radio"
+          value="small"
+          onChange={handleChange}
+          checked={formState['size'] === 'small'}
+        />
+        <label>Small</label>
+        <input
+          id="size"
+          type="radio"
+          value="medium"
+          onChange={handleChange}
+          checked={formState['size'] === 'medium'}
+        />
+        <label>Medium</label>
+        <input
+          id="size"
+          type="radio"
+          value="large"
+          onChange={handleChange}
+          checked={formState['size'] === 'large'}
+        />
+        <label>Large</label>
+      </div>
+      <div>Topping:
+        <select id="topping" onChange={handleChange}>
+          <option
+            id="topping"
+            value="pepperoni"
+          >Pepperoni</option>
+          <option
+            id="topping"
+            value="cheese"
+          >Cheese</option>
+        </select>
+      </div>
+      <div>Gluten free:
+        <input
+          type="checkbox"
+          id="glutenFree"
+          checked={formState.glutenFree}
+          onChange={handleCheck}
+        />
+      </div>
+      <div>Special instructions:
+        <input
+          id="notes"
+          value={formState.notes}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <button onClick={handleSubmit}>Submit</button>
+      </div>
+    </>
+    )
+}
+
+root.render(<PizzaForm />)
