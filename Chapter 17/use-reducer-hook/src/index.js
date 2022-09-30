43cf229efdfc0aa639e18fcaa783d0100037cd77
Ashik Varghese <ashikv@nulogy.com>
2022-09-29 22:43:39 -0400

    Compare a conventional vs reducer component

--- Chapter 17/use-reducer-hook/src/index.js
+++ Chapter 17/use-reducer-hook/src/index.js
@@ -1,29 +1,54 @@
-import React, {useReducer} from 'react'
+import React, {useReducer, useState} from 'react'
 import ReactDOM from 'react-dom'
 
 // reducer in js
-const adder = (total, number) => {
-  return total + number
+function adder(total, number) {
+  return number + total
 }
 
-const numbers = [1, 2, 3]
+console.log([1, 2, 3].reduce(adder, 0))
 
-console.log(numbers.reduce(adder, 0))
+const ConventionalCounter = () => {
 
-const Counter = () => {
-  const [sum, dispatch] = useReducer((state, action) => {
-    return state + action
-  }, 0)
+  const [number, setNumber] = useState(0) // state is defined
+
+  const addOne = () => setNumber(number + 1) // setNumber is a rather simple function that only does one thing to the state
+
+  const removeOne = () => setNumber(number - 1) // if there was another action to be done, you'd need another onClick handler
 
   return (
     <>
-      {sum}
-      <button onClick={() => dispatch(1)}>Add 1</button>
+      <div>{number}</div>
+      <button onClick={addOne}>Add 1</button>
+      <button onClick={removeOne}>Remove 1</button>
+    </>
+  )
+}
+
+const ReducerCounter = () => {
+  const reducer = (state, action) => {
+    switch(action) {
+      case 'addOne': // the reducer makes it really easy to manage actions
+        return state + 1
+      case 'removeOne': // you can just add a new one as needed
+        return state - 1
+      default:
+        return state
+    }
+  }
+
+  const [number, dispatch] = useReducer(reducer, 0)
+
+  return (
+    <>
+      <div>{number}</div>
+      <button onClick={() => dispatch('addOne')}>Add 1</button>
+      <button onClick={() => dispatch('removeOne')}>Remove 1</button>
     </>
   )
 }
 
 ReactDOM.render(
-  <Counter />,
+  <ReducerCounter />,
   document.querySelector('#root')
 )
