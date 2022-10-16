ec7f53adb4437c88734ab956540b2b6c0897514f
Ashik Varghese <ashikv@nulogy.com>
2022-10-15 22:48:25 -0400

    add more useEffect examples and some questions

--- Chapter 18/use-effect-hook-lesson/src/index.js
+++ Chapter 18/use-effect-hook-lesson/src/index.js
@@ -1,10 +1,13 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import ReactDOM from 'react-dom'
 
 const LogEffect = () => {
   const [text, setText] = useState('')
   useEffect(
-    () => console.log('the latest value is:', text) // run this effect every time there is a new render (change to the state)
+    () => {
+      console.log('the latest value is:', text) // run this effect every time there is a new render (change to the state)
+      return () => console.log('clean up') // called when the component unmounts or before the effect is called; not called during mount
+    }
   )
 
   return <input value={text} onChange={(e) => setText(e.target.value)} /> // controlled input
@@ -21,7 +24,55 @@ const LogEffectOnMountOnly = () => {
   return <input value={text} onChange={(e) => setText(e.target.value)} />
 }
 
+const LogEffectWhenPassword = () => {
+  const [pass, setPass] = useState('')
+  const inputRef = useRef() // this will be used to auto-focus the cursor on the input box upon render
+  const [showSecretMessage, setShowSecretMessage] = useState(false)
+  useEffect(
+    () => {
+      setShowSecretMessage(pass === 'passcode')
+      console.log(inputRef.current.value)  //~ inputRef is changing
+    },
+    [pass] // only runs the side-effect when the 'pass' state is changed
+  )
+  useEffect(
+    () => {
+      console.log('inputRef changed?') //~but if it's changing why is this not getting rendered
+      inputRef.current.focus()
+    },
+    [inputRef] // only runs the side-effect when 'inputRef' is changed which is never after the first render
+  )
+
+  return (
+    <>
+      <div>guess passcode...</div>
+      <input type="text" value={pass} onChange={(e) => setPass(e.target.value)} ref={inputRef}/>
+      {showSecretMessage && <div>Secret revealed!</div>}
+    </>
+  )
+}
+
+const LogEffectWhenPasswordTerse = () => {
+  const [pass, setPass] = useState({ enteredValue: '', showSecret: false})
+  useEffect(
+    () => {
+      if(pass.enteredValue === 'passcode') {
+        setPass({...pass, showSecret: true})
+      }
+    },
+    [pass]
+  )
+
+  return (
+    <>
+      <div>guess passcode...</div>
+      <input type="text" value={pass.enteredValue} onChange={(e) => setPass({...pass, enteredValue: e.target.value})} />
+      {pass.showSecretMessage && <div>Secret revealed!</div>}
+    </>
+  )
+}
+
 ReactDOM.render(
-  <LogEffectOnMountOnly />,
+  <LogEffectWhenPassword />,
   document.querySelector('#root')
 )
